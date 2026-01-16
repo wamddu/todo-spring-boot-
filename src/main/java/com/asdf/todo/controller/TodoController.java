132c7171c8d41ef8476fd7991afe309de945287f
wamddu <yhy6923@naver.com>
2026-01-16 16:18:12 +0900

    Todo 목록 가져오기 기능 구현

--- src/main/java/com/asdf/todo/controller/TodoController.java
+++ src/main/java/com/asdf/todo/controller/TodoController.java
@@ -1,4 +1,54 @@
 package com.asdf.todo.controller;
 
+import com.asdf.todo.model.Todo;
+import com.asdf.todo.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/todos/v1")
 public class TodoController {
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping
+    @Operation(summary = "전체 작업 조회", description = "전체 작업 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "204", description = "내용 없음")
+    })
+    public ResponseEntity<List<Todo>> getAllTodos() {
+        List<Todo> todos = todoService.findAll();
+        if (todos == null || todos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(todos);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "작업 조회", description = "ID로 작업 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "204", description = "작업 없음")
+    })
+    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+        Todo todo = todoService.findById(id);
+        if(todo == null)
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(todo);
+    }
+
+
 }
