b25032856c527a24662b31fa543e21dbbbfe8000
wamddu <yhy6923@naver.com>
2026-01-22 11:29:55 +0900

    Dto에 맞게 Controller 수정

--- src/main/java/com/asdf/todo/controller/TodoController.java
+++ src/main/java/com/asdf/todo/controller/TodoController.java
@@ -1,11 +1,12 @@
 package com.asdf.todo.controller;
 
-import com.asdf.todo.model.Todo;
+import com.asdf.todo.dto.TodoRequestDto;
+import com.asdf.todo.dto.TodoResponseDto;
+import com.asdf.todo.entity.Todo;
 import com.asdf.todo.service.TodoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/todos/v1")
+@RequestMapping("/api/todos/v2")
 public class TodoController {
     @Autowired
     private TodoService todoService;
@@ -25,8 +26,8 @@ public class TodoController {
             @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "204", description = "내용 없음")
     })
-    public ResponseEntity<List<Todo>> getAllTodos() {
-        List<Todo> todos = todoService.findAll();
+    public ResponseEntity<List<TodoResponseDto>> getAllTodos() {
+        List<TodoResponseDto> todos = todoService.findAll();
         if (todos == null || todos.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
@@ -39,27 +40,12 @@ public class TodoController {
             @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "204", description = "작업 없음")
     })
-    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
-        Todo todo = todoService.findById(id);
+    public ResponseEntity<TodoResponseDto> getTodoById(@PathVariable Long id) {
+        TodoResponseDto todo = todoService.findById(id);
         if(todo == null)
             return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok(todo);
-    }
-
-    @DeleteMapping("/{id}")
-    @Operation(summary = "작업 삭제", description = "ID로 작업 삭제")
-    @ApiResponses({
-            @ApiResponse(responseCode = "204", description = "내용 없음"),
-            @ApiResponse(responseCode = "404", description = "작업 없음")
-    })
-    public ResponseEntity<Void> deleteTodoById(@PathVariable Long id) {
-        Todo todo  = todoService.findById(id);
-        if(todo == null)
-            return ResponseEntity.notFound().build();
-
-        todoService.delete(id);
-        return ResponseEntity.noContent().build();
     }
 
     @PostMapping
@@ -67,7 +53,7 @@ public class TodoController {
     @ApiResponses({
             @ApiResponse(responseCode = "201", description ="생성됨")
     })
-    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
+    public ResponseEntity<TodoResponseDto> createTodo(@RequestBody TodoRequestDto todo) {
         return ResponseEntity.status(201).body(todoService.save(todo));
     }
 
@@ -77,9 +63,9 @@ public class TodoController {
             @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "404", description = "작업 없음")
     })
-    public ResponseEntity<Todo> updateTodo(@PathVariable Long id,
-                                           @RequestBody Todo todo) {
-        Todo existingTodo = todoService.findById(id);
+    public ResponseEntity<TodoResponseDto> updateTodo(@PathVariable Long id,
+                                           @RequestBody TodoRequestDto todo) {
+        TodoResponseDto existingTodo = todoService.findById(id);
         if(existingTodo == null)
             return ResponseEntity.notFound().build();
 
@@ -93,7 +79,7 @@ public class TodoController {
             @ApiResponse(responseCode = "404", description = "작업 없음")
     })
     public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
-        Todo todo = todoService.findById(id);
+        TodoResponseDto todo = todoService.findById(id);
         if(todo == null)
             return  ResponseEntity.notFound().build();
 
