96d9f08c8042a039623a1aa025e6e3b584d58e1b
wamddu <yhy6923@naver.com>
2026-01-22 11:30:49 +0900

    DTO, DTO Mapper 구현

---
+++ src/main/java/com/asdf/todo/dto/TodoResponseDto.java
@@ -0,0 +1,21 @@
+package com.asdf.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoResponseDto {
+
+    @NonNull
+    private Long id;
+
+    @NonNull
+    private String title;
+
+    private String description;
+    private boolean completed;
+}
